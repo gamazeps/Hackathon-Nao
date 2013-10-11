2a1abaf6009f89a87c376e71ce81994e5b766586
gamazeps <felix.raimundo@telecom-paristech.fr>
2013-10-11 18:57:41 +0200

    connexion avec la camera etablie

---
+++ cards_processing.py
@@ -0,0 +1,36 @@
+# -*- encoding: UTF-8 -*-
+
+# This is just an example script that shows how images can be accessed
+# through ALVideoDevice in python.
+# Nothing interesting is done with the images in this example.
+
+from naoqi import ALProxy
+import vision_definitions
+
+IP = "nao.local"  # Replace here with your NAOqi's IP address.
+PORT = 9559
+
+####
+# Create proxy on ALVideoDevice
+
+print "Creating ALVideoDevice proxy to ", IP
+
+camProxy = ALProxy("ALVideoDevice", IP, PORT)
+
+####
+# Register a Generic Video Module
+
+resolution = vision_definitions.kQVGA
+colorSpace = vision_definitions.kHSYColorSpace
+fps = 15
+
+nameId = camProxy.subscribe("python_GVM", resolution, colorSpace, fps)
+print nameId
+
+
+print 'getting an image in remote'
+camProxy.getImageRemote(nameId)
+
+camProxy.unsubscribe(nameId)
+
+print 'end of gvm_getImageLocal python script'
