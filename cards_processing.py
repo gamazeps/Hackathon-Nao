f738f1a8b11c2d900b03496bdbbbc6f2ced26cb7
gamazeps <felix.raimundo@telecom-paristech.fr>
2013-10-12 15:23:05 +0200

    union plus correcte, fonction pour avoir les cartes en fonction des centres implementee

--- cards_processing.py
+++ cards_processing.py
@@ -4,37 +4,44 @@
 # through ALVideoDevice in python.
 # Nothing interesting is done with the images in this example.
 
-from naoqi import ALProxy
-import vision_definitions
+#from naoqi import ALProxy
+#import vision_definitions
 import numpy as np
 
 IP = "nao.local"  # Replace here with your NAOqi's IP address.
 PORT = 9559
 
+# Gives out a list containing the hsv coordinates of a pixel from the raw input of the camera
 def parse_hsy(data):
 	bytearray_data = bytearray.fromhex(str(hex(data))[2::])
 	hsy = [bytearray_data[2], bytearray_data[1], bytearray_data[0]]
 	return hsy
 
+# Applies a basic threshold to an hsv pixel
 def hsv_threshold(source, data, dist=10):
-	if(abs(source[0] - data[0]) > dist):
+	if (abs(source[0] - data[0]) > dist):
 		return false
-	else if(abs(source[1] - data[1]) > dist):
+	elif (abs(source[1] - data[1]) > dist):
 		return false
-	else if(abs(source[2] - data[2]) > dist):
+	elif (abs(source[2] - data[2]) > dist):
 		return false
 	else:
 		return true
 
-def get2dArray(data, width=120, length=160):
-	matrix = np.zeros([length, width], dtype = int)
-	for i in range(width*length):
-		matrix[i%width][i/length] = data[i]
-	return matrix
+# Returns a 2D array from a 1D array, given the width 
+def get2d_array(data, width=120, length=160):
+	if len(data) == width * length:
+		matrix = np.zeros([length, width], dtype = int)
+		for i in range(width*length):
+			matrix[i%width][i/length] = data[i]
+		return matrix
+	else:
+		print "The size of the 2D array does not match the size of the 1D array"
 
-
+# Finds the connex components in a binary 2D array, the data structure employed is a lazy union-find one,
+# is executed in a O(n*ln(n)) complexity in the average and worst case (where n is the numpber of pixels) 
+# and in best case scenario in O(n) 
 def connex_components(logic_matrix, width = 120, length = 160):
-	## The case where j=0 isn't treated but i am honestly too lazy to care about it now
 	components = []
 	components_id = np.zeros([length, width], dtype = int)
 	id_count = 1
@@ -53,60 +60,96 @@ def connex_components(logic_matrix, width = 120, length = 160):
 				id_count = id_count + 1
 		for j in range(1, width):
 			if logic_matrix[i][j]:
-				if logic_matrix[i-1][j] && logic_matrix[i][j - 1]:
+				if logic_matrix[i-1][j] and logic_matrix[i][j - 1]:
 					if components_id[i - 1][j] != components_id[i][j - 1]:
 						union(components, components_id, components_id[i - 1][j], components_id[i][j - 1])
 					else:
 						components[components_id[i - 1][j]].append([i][j])
 						components_id[i][j] = components_id[i - 1][j]
 					pass
-				else if logic_matrix[i - 1][j]:
+				elif logic_matrix[i - 1][j]:
 					components[components_id[i - 1][j]].append([i][j])
 					components_id[i][j] = components_id[i - 1][j]
-				else if logic_matrix[i][j - 1]:
+				elif logic_matrix[i][j - 1]:
 					components[components_id[i][j - 1]].append([i][j])
 					components_id[i][j] = components_id[i][j - 1]
 				else:
 					components.append([i][j])
 					components_id[i][j] = id_count
 					id_count = id_count + 1
+	# Here we remove all the empty components and the one too small to be cards
+	lists_to_remove = []
+	for component in components:
+		if len(component) < 1:
+			lists_to_remove.append(component)
+	for i in range(lists_to_remove):
+		components.remove(component)
+
 	return components
 
+# Realises the union action in the union-find datastructure
 def union(components, components_id, id1, id2):
-	if len(components[id1])>len(components[id2]):
+	if len(components[id1]) > len(components[id2]):
 		for k in components[id2]:
-			components_id[k[1]][k[2]] = id1
-			components[id2].remove(k)
+			components_id[k[0]][k[1]] = id1
 			components[id1].append(k)
+		components[id2] = []
 	else:
 		for k in components[id1]:
-			components_id[k[1]][k[2]] = id2
-			components[id1].remove(k)
+			components_id[k[0]][k[1]] = id2
 			components[id2].append(k)
+		components[id1] = []
 
+# Returns the center of a group of pixels
+def get_centers(components):
+	centers = []
+	for component in components:
+		i = 0
+		j = 0
+		for pixel in component:
+			i = i + pixel[0]
+			j = j + pixel[1]
+		centers.append([i/len(component), j/len(component)])
+	return centers
 
+def get_cards_pos(blue_cards_components, red_cards_components, yellow_cards_components):
+	blue_centers = get_centers(blue_cards_components)
+	red_centers = get_centers(red_cards_components)
+	yellow_centers = get_centers(yellow_cards_components)
+	color_and_pos = []
+	for card in blue_centers:
+		color_and_pos.append([card[0], 'b'])
+	for card in red_centers:
+		color_and_pos.append([card[0], 'r'])
+	for card in yellow_centers:
+		color_and_pos.append([card[0], 'y'])
+	color_and_pos.sort()
+	cards = []
+	for card in color_and_pos:
+		cards.append(card[1])
+	return cards
 
 ####
 # Create proxy on ALVideoDevice
 
-print "Creating ALVideoDevice proxy to ", IP
+#print "Creating ALVideoDevice proxy to ", IP
 
-camProxy = ALProxy("ALVideoDevice", IP, PORT)
+# = ALProxy("ALVideoDevice", IP, PORT)
 
 ####
 # Get camera frame
 
-resolution = vision_definitions.kQVGA
-colorSpace = vision_definitions.kHSYColorSpace
+#resolution = vision_definitions.kQVGA
+#colorSpace = vision_definitions.kHSYColorSpace
 fps = 15
 
-nameId = camProxy.subscribe("python_GVM", resolution, colorSpace, fps)
-print nameId
+#nameId = camProxy.subscribe("python_GVM", resolution, colorSpace, fps)
+#print nameId
 
 
 print 'getting an image in remote'
-image = camProxy.getImageRemote(nameId)[6]
+#image = camProxy.getImageRemote(nameId)[6]
 
-camProxy.unsubscribe(nameId)
+#camProxy.unsubscribe(nameId)
 
 print 'end of gvm_getImageLocal python script'
